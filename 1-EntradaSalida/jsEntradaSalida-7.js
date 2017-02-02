8a6a9be09c3726e2ef74d405cb35785bae426218
alumno <alumno>
2017-02-02 10:20:10 -0300

    Ejercicio 7 terminado

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,53 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var a;
+	var b;
+	var resultado;
+	a = document.getElementById("numeroUno").value;
+	b = document.getElementById("numeroDos").value;
+	a = parseInt (a);
+	b = parseInt (b);
+	resultado = a + b;
+	alert ("La suma es "+resultado);
 }
 
 function restar()
 {
-	
+	var a;
+	var b;
+	var resultado;
+	a = document.getElementById("numeroUno").value;
+	b = document.getElementById("numeroDos").value;
+	a = parseInt (a);
+	b = parseInt (b);
+	resultado = a - b;
+	alert ("La resta es "+resultado);
 }
 
 function multiplicar()
 { 
-	
+	var a;
+	var b;
+	var resultado;
+	a = document.getElementById("numeroUno").value;
+	b = document.getElementById("numeroDos").value;
+	a = parseInt (a);
+	b = parseInt (b);
+	resultado = a * b;
+	alert ("La multiplicacion es "+resultado);
 }
 
 function dividir()
 {
-	
+	var a;
+	var b;
+	var resultado;
+	a = document.getElementById("numeroUno").value;
+	b = document.getElementById("numeroDos").value;
+	a = parseInt (a);
+	b = parseInt (b);
+	resultado = a / b;
+	alert ("La division es "+resultado);
 }
 
