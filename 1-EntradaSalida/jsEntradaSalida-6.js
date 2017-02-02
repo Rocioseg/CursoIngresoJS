280cef4c367aec6c68e9208447b79179e490d52e
alumno <alumno>
2017-02-02 10:04:17 -0300

    Ejercicio 6 terminado

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,14 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-
+	var a;
+	var b;
+	var result;
+	a = document.getElementById("numeroUno").value;
+	b = document.getElementById("numeroDos").value;
+	a = parseInt(a);
+	b = parseInt(b);
+	result= a + b
+	alert ("La suma es "+result);
 }
 
