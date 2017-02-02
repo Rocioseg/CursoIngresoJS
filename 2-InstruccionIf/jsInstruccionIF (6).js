8dba0d8a8c79da7fad169fdaa758b8d473d30e98
alumno <alumno>
2017-02-02 12:23:09 -0300

    If Ejercicio 6 terminado

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,10 @@
 function Mostrar()
 {
-//tomo la edad  
-
-
-
-
+	var a;
+	a = document.getElementById("edad").value;
+	if (a >= 18)
+		{alert ("La persona es mayor de edad")}
+	else if (a < 13)
+		{alert ("La persona es un niño/niña")}
+	else {alert ("La persona es adolescente")}
 }//FIN DE LA FUNCIÓN
