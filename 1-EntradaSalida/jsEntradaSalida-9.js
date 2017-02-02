976beb821f79575c7787a03876c89677ec8e3459
alumno <alumno>
2017-02-02 11:20:26 -0300

    Ejercicio 9 terminado

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,9 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-
-	
+	var a;
+	a = document.getElementById("sueldo").value;
+	a = parseInt(a);
+	document.getElementById("resultado").value = (a * 0.1) + a;
 }
+/*Se puede hacer: si sumas el porcentaje al sueldo, la variable2 igual a la variable1 por 1.10*/
