752cc81a3ccfa5c36c93fbed6c0606f220b62c6c
alumno <alumno>
2017-02-02 11:31:15 -0300

    Ejercicio 10 terminado

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,9 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var a;
+	a = document.getElementById("importe").value;
+	a = parseInt(a);
+	document.getElementById("resultado").value = a - (a * 0.25);
 }
+/*para restarle el porciento hay que poner la variable "a" adelante*/
