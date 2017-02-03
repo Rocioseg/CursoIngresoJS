1d3b027b7a26708dbee6f83fa1555936206cdc6e
rocioseg <rocio.segurola97@gmail.com>
2017-02-03 01:00:01 -0300

    If Ejercicio 6 CORREGIDO (sin elseif)

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,9 +2,21 @@ function Mostrar()
 {
 	var a;
 	a = document.getElementById("edad").value;
+	a = parseInt (a); //convertir a numero//
 	if (a >= 18)
-		{alert ("La persona es mayor de edad")}
-	else if (a < 13)
-		{alert ("La persona es un niño/niña")}
-	else {alert ("La persona es adolescente")}
+	{
+	 alert ("La persona es mayor de edad");
+    }
+    else
+    {
+    	if (a < 13) // no poner punto y coma luego del if//
+    	{
+    		alert ("La persona es un niño/niña");
+    	}
+    	else
+    	{
+    		alert ("La persona es adolescente");
+    	}
+    }
+
 }//FIN DE LA FUNCIÓN
