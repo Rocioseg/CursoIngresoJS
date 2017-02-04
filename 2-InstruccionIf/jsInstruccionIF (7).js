bb8f199413f48c4560370945d8e4dada61366e90
rocioseg <rocio.segurola97@gmail.com>
2017-02-04 17:51:13 -0300

    If Ejercicio 7 terminado

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,12 @@
 function Mostrar()
 {
-//tomo la edad  
-
-	
-
-
+	var a;
+	var estado;
+	a = document.getElementById("edad").value;
+	a = parseInt (a);
+	estado = document.getElementById("estadoCivil").value;
+	if (a < 18 && !(estado == "Soltero"))
+	{
+		alert("Es muy joven para NO ser soltero");
+	}
 }//FIN DE LA FUNCIÓN
