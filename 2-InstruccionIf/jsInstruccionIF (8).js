8f8d5332249e7268226806feb7eebf06189966b5
rocioseg <rocio.segurola97@gmail.com>
2017-02-05 20:41:38 -0300

    If Ejercicio 8 terminado

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,10 @@
 function Mostrar()
 {
-//tomo la edad  
-
-	
-
-
+	var edad;
+	var estado;
+	edad = document.getElementById("edad").value;
+	edad = parseInt (edad)
+	estado = document.getElementById("estadoCivil").value;
+	if (edad >= 18 && estado == "Soltero")
+		alert ("Es soltero y no es menor")
 }//FIN DE LA FUNCIÓN
