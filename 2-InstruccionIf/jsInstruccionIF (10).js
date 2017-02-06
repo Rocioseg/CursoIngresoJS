4badd2c2f491e356d2a9cdaba9430c92c4dd018e
rocioseg <rocio.segurola97@gmail.com>
2017-02-05 21:10:34 -0300

    If Random Ejercicio 10 terminado

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,11 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
-
+	var numero;
+	numero = Math.round (Math.random () * 10);
+	if (numero >= 9)
+		alert ("LA NOTA ES "+numero+" ,EXCELENTE")
+	else if (numero >= 4)
+		alert ("LA NOTA ES "+numero+" ,APROBO")
+	else
+		alert ("LA NOTA ES "+numero+" ,VAMOS, LA PROXIMA SE PUEDE");
 }//FIN DE LA FUNCIÓN
