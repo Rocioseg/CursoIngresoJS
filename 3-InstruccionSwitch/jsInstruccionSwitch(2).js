3a815bd7351e5cb0834939135f9240908db4d71d
alumno <alumno>
2017-02-08 11:40:50 -0300

    Switch Ejercicio 2 terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,23 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
-
-
-
-
+var mes = document.getElementById("mes").value;
+switch (mes){
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	{
+		alert ("Ya paso el invierno!")
+	}break;
+	case "Julio":
+	case "Agosto":
+	{
+		alert ("Abrigate que hace frio!")
+	}break;
+	default:
+	{
+		alert ("Falta para el invierno")
+	}break;
+}
 }//FIN DE LA FUNCIÓN
