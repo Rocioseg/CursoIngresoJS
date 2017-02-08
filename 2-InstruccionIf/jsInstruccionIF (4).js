6f59945b223e0e11ff21c70c62c36f49d09e6946
alumno <alumno>
2017-02-08 11:39:01 -0300

    If Ejercicio 4 CORREGIDO terminado

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -3,5 +3,7 @@ function Mostrar()
 	var a;
 	a = document.getElementById("edad").value;
 	if ( a >= 13 && a <= 17)
-		{alert ("Esta persona es adolescente")}
+		{
+			alert ("Esta persona es adolescente")
+		}
 }//FIN DE LA FUNCIÓN
