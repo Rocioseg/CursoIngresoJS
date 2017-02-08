59d173bce72ddea75428d5d7e0c82aab2fffd50c
alumno <alumno>
2017-02-08 11:41:18 -0300

    Switch Ejercicio 4 terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,22 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-//alert (mesDelAño);
-	
-	
-
-
-
+var mes = document.getElementById("mes").value;
+switch (mes)
+{
+	case "Abril":
+	case "Junio":
+	case "Septiembre":
+	case "Noviembre":
+	{
+		alert ("Tiene 30 dias");
+	}break;
+	case "Febrero":
+	{
+		alert ("Tiene 28 dias");
+	}break;
+	default:
+	{
+		alert ("Tiene 31 dias");
+	}break;
+}
 }//FIN DE LA FUNCIÓN
