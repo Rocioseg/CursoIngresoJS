ecca8a1be821f977ed031542f871193787b9cb85
alumno <alumno>
2017-02-08 11:39:41 -0300

    If Ejercicio 5 CORREGIDO terminado

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -2,6 +2,7 @@ function Mostrar()
 {
 	var a
 	a = document.getElementById("edad").value;
+	a = parseInt (a)
 	if (a < 13 || a > 17)
 		{alert ("Esta persona NO es adolescente")}
 }//FIN DE LA FUNCIÓN
