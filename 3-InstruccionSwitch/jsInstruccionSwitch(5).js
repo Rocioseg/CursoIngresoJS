876c2e49d1c13edd74ec4b5ef8c8d69e7eebc1b5
alumno <alumno>
2017-02-08 11:41:35 -0300

    Switch Ejercicio 5 terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,15 @@
 function Mostrar()
+{ 
+var hora = document.getElementById("hora").value;
+switch (hora)
 {
-//tomo la edad  
-var laHora = document.getElementById('hora').value;
-
-//alert (laHora);
-	
-	
-
-
-
+	case "7":
+	case "8":
+	case "9":
+	case "10":
+	case "11":
+	{
+		alert ("Es de mañana")
+	}break;
+}
 }//FIN DE LA FUNCIÓN
