38ef7f451c74b465d0082149bf9f31ffbc00a7ea
alumno <alumno>
2017-02-08 11:39:22 -0300

    If Ejercicio 10 CORREGIDO terminado

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,11 +1,18 @@
 function Mostrar()
 {
 	var numero;
-	numero = Math.round (Math.random () * 10);
+	numero = parseInt (Math.random() * (11 - 1) + 1);
 	if (numero >= 9)
 		alert ("LA NOTA ES "+numero+" ,EXCELENTE")
-	else if (numero >= 4)
-		alert ("LA NOTA ES "+numero+" ,APROBO")
-	else
-		alert ("LA NOTA ES "+numero+" ,VAMOS, LA PROXIMA SE PUEDE");
+	else 
+	{	
+		if (numero >= 4)
+		{
+			alert ("LA NOTA ES "+numero+" ,APROBO")
+		}
+		else
+		{
+			alert ("LA NOTA ES "+numero+" ,VAMOS, LA PROXIMA SE PUEDE");
+		}
+	}
 }//FIN DE LA FUNCIÓN
